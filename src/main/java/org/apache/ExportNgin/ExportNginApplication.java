57288fb94431c98d839cbf1b8a52316b83b6fb45
Brook Mesfin <Brook@PeopleFinders.com>
2016-09-23 11:11:24 -0700

    Project Creation initial commit

---
+++ src/main/java/org/apache/ExportNgin/ExportNginApplication.java
@@ -0,0 +1,12 @@
+package org.apache.ExportNgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportNginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportNginApplication.class, args);
+	}
+}
